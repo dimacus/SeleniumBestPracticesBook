e356e9cbb4a774228f63d2146de21d24ee465d7a
Dima Kovalenko <dima@groupon.com>
2014-09-10 12:36:40 -0400

    Organizing all of the files into better easy to understand structure

--- Chapter 7/Ruby/page_objects/home_page/special_item.rb
+++ Chapter 7/Ruby/page_objects/home_page/special_item.rb
@@ -4,9 +4,8 @@ class SpecialItem
       @selenium = selenium
    end
    
-   def add_to_cart
+   def add_to_cart 
      @element.find_element(:class, "add-to-cart").click  
      @selenium.find_element(:id, "fancybox-outer").find_element(:class, "purAddToCart").click
-     ShoppingCartPage.new(@selenium)
    end        
 end
--- Chapter 7/Ruby/page_objects/requires.rb
+++ Chapter 7/Ruby/page_objects/requires.rb
@@ -5,9 +5,12 @@ require 'test/unit'
 require 'selenium-webdriver'
 require 'nokogiri'
 
-require File.expand_path(File.join(File.dirname(__FILE__),  "page"))
-require File.expand_path(File.join(File.dirname(__FILE__),  "home_page"))
-require File.expand_path(File.join(File.dirname(__FILE__),  "contact_us_page"))
-require File.expand_path(File.join(File.dirname(__FILE__),  "sidebar"))
-require File.expand_path(File.join(File.dirname(__FILE__),  "sidebar_cart"))
-require File.expand_path(File.join(File.dirname(__FILE__),  "special_item"))
+require File.expand_path(File.join(File.dirname(__FILE__), "shared" , "page"))
+
+require File.expand_path(File.join(File.dirname(__FILE__), "home_page", "home_page"))
+require File.expand_path(File.join(File.dirname(__FILE__), "home_page", "special_item"))
+
+require File.expand_path(File.join(File.dirname(__FILE__), "contact_us_page", "contact_us_page"))
+require File.expand_path(File.join(File.dirname(__FILE__), "shared", "sidebar", "sidebar"))
+require File.expand_path(File.join(File.dirname(__FILE__), "shared", "sidebar", "sidebar_cart"))
+
